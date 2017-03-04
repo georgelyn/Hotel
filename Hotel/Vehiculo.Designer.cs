6757248c78173a3538381f0f6b1cf43059a8bb70
georgelyn <30326226+georgelyn@users.noreply.github.com>
2017-03-04 14:21:26 -0400

    Creada clase para vehículos - Ver, modificar y Eliminar. Buscar por cédula cliente.

---
+++ Hotel/Vehiculo.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Hotel
+{
+    partial class Vehiculo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboVehiculos = new System.Windows.Forms.ComboBox();
+            this.checkCamion = new System.Windows.Forms.CheckBox();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.lblPlaca = new System.Windows.Forms.Label();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.comboCedula = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNotas = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vehículos asociados con cédula:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Elija uno de la lista:";
+            // 
+            // comboVehiculos
+            // 
+            this.comboVehiculos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboVehiculos.FormattingEnabled = true;
+            this.comboVehiculos.Location = new System.Drawing.Point(243, 78);
+            this.comboVehiculos.Name = "comboVehiculos";
+            this.comboVehiculos.Size = new System.Drawing.Size(266, 28);
+            this.comboVehiculos.TabIndex = 0;
+            this.comboVehiculos.SelectedIndexChanged += new System.EventHandler(this.comboVehiculos_SelectedIndexChanged);
+            // 
+            // checkCamion
+            // 
+            this.checkCamion.AutoSize = true;
+            this.checkCamion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkCamion.Location = new System.Drawing.Point(525, 150);
+            this.checkCamion.Name = "checkCamion";
+            this.checkCamion.Size = new System.Drawing.Size(82, 24);
+            this.checkCamion.TabIndex = 49;
+            this.checkCamion.Text = "Camión";
+            this.checkCamion.UseVisualStyleBackColor = true;
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(368, 149);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(124, 26);
+            this.txtPlaca.TabIndex = 1;
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMarca.Location = new System.Drawing.Point(73, 151);
+            this.lblMarca.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(57, 20);
+            this.lblMarca.TabIndex = 55;
+            this.lblMarca.Text = "Marca:";
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(141, 201);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(351, 26);
+            this.txtModelo.TabIndex = 2;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(141, 148);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(156, 26);
+            this.txtMarca.TabIndex = 0;
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModelo.Location = new System.Drawing.Point(73, 204);
+            this.lblModelo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(65, 20);
+            this.lblModelo.TabIndex = 56;
+            this.lblModelo.Text = "Modelo:";
+            // 
+            // lblPlaca
+            // 
+            this.lblPlaca.AutoSize = true;
+            this.lblPlaca.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlaca.Location = new System.Drawing.Point(309, 151);
+            this.lblPlaca.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPlaca.Name = "lblPlaca";
+            this.lblPlaca.Size = new System.Drawing.Size(52, 20);
+            this.lblPlaca.TabIndex = 57;
+            this.lblPlaca.Text = "Placa:";
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(462, 6);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(95, 35);
+            this.btnModificar.TabIndex = 58;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Visible = false;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(563, 6);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(95, 35);
+            this.btnCancelar.TabIndex = 59;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightBlue;
+            this.panel1.Controls.Add(this.btnEliminar);
+            this.panel1.Controls.Add(this.btnModificar);
+            this.panel1.Controls.Add(this.btnCancelar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 357);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(666, 51);
+            this.panel1.TabIndex = 60;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Red;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEliminar.Location = new System.Drawing.Point(16, 6);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(87, 34);
+            this.btnEliminar.TabIndex = 60;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Visible = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // comboCedula
+            // 
+            this.comboCedula.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboCedula.FormattingEnabled = true;
+            this.comboCedula.Location = new System.Drawing.Point(314, 34);
+            this.comboCedula.Name = "comboCedula";
+            this.comboCedula.Size = new System.Drawing.Size(195, 28);
+            this.comboCedula.TabIndex = 61;
+            this.comboCedula.SelectedIndexChanged += new System.EventHandler(this.comboCedula_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(73, 253);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 62;
+            this.label2.Text = "Notas:";
+            // 
+            // txtNotas
+            // 
+            this.txtNotas.Location = new System.Drawing.Point(141, 250);
+            this.txtNotas.Name = "txtNotas";
+            this.txtNotas.Size = new System.Drawing.Size(351, 73);
+            this.txtNotas.TabIndex = 63;
+            this.txtNotas.Text = "";
+            // 
+            // Vehiculo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 408);
+            this.Controls.Add(this.txtNotas);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboCedula);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.checkCamion);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.comboVehiculos);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.lblModelo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPlaca);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Vehiculo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vehiculo";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboVehiculos;
+        private System.Windows.Forms.CheckBox checkCamion;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.Label lblPlaca;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.ComboBox comboCedula;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtNotas;
+    }
+}
---
+++ Hotel/Vehiculo.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Hotel
+{
+    public partial class Vehiculo : Form
+    {
+        public Vehiculo()
+        {
+            InitializeComponent();
+
+            CargarCedulas();
+            //CargarVehiculosPorCedula("25036451");
+        }
+
+        Msg msg;
+
+        List<String> idVehiculo; // Toma valor en CargarVehiculosPorCedula
+
+
+        private void CargarCedulas()
+        {
+            try
+            {
+                comboCedula.Items.Clear();
+                comboVehiculos.Items.Clear();
+
+                using (SQLiteConnection conn = new SQLiteConnection(ConexionBD.connstring))
+                {
+                    using (SQLiteCommand cmd = new SQLiteCommand("SELECT DISTINCT cedula FROM cliente INNER JOIN vehiculo ON cedula=cedula_cliente", conn))
+                    {
+                        conn.Open();
+
+                        using (SQLiteDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+                                comboCedula.Items.Add(dr["cedula"].ToString());
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se ha presentado un error.\nDetalles:\n\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CargarVehiculosPorCedula(string cedula)
+        {
+            comboVehiculos.Items.Clear();
+
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(ConexionBD.connstring))
+                {
+                    using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM vehiculo WHERE cedula_cliente=@cedula", conn))
+                    {
+                        idVehiculo = new List<String>();
+                        idVehiculo.Clear();
+
+                        cmd.Parameters.AddWithValue("@cedula", cedula);
+                        conn.Open();
+
+                        using (SQLiteDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+                                idVehiculo.Add(dr["id"].ToString());
+                                comboVehiculos.Items.Add(dr["marca"].ToString() + " - " + dr["modelo"].ToString());
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se ha presentado un error.\nDetalles:\n\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CargarDatosVehiculo(string id)
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(ConexionBD.connstring))
+                {
+                    using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM vehiculo WHERE id=@id", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+
+                        conn.Open();
+
+                        using (SQLiteDataReader dr = cmd.ExecuteReader())
+                        {
+                            while (dr.Read())
+                            {
+                                txtMarca.Text = dr["marca"].ToString();
+                                txtModelo.Text = dr["modelo"].ToString();
+                                txtPlaca.Text = dr["placa"].ToString();
+                                txtNotas.Text = dr["notas"].ToString();
+
+                                if (Convert.ToBoolean(dr["es_camion"]) == true)
+                                    checkCamion.Checked = true;
+                                else
+                                    checkCamion.Checked = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se ha presentado un error.\nDetalles:\n\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ModificarVehiculo(string id)
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(ConexionBD.connstring))
+                {
+                    using (SQLiteCommand cmd = new SQLiteCommand("UPDATE vehiculo SET es_camion=@esCamion, marca=@marca, modelo=@modelo, placa=@placa, notas=@notas WHERE id=@id", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+
+                        bool esCamion = false;
+
+                        if (checkCamion.Checked)
+                            esCamion = true;
+
+                        cmd.Parameters.AddWithValue("@esCamion", esCamion);
+
+                        cmd.Parameters.AddWithValue("@marca", StringExtensions.NullString(txtMarca.Text.Trim()));
+                        cmd.Parameters.AddWithValue("@modelo", StringExtensions.NullString(txtModelo.Text.Trim()));
+                        cmd.Parameters.AddWithValue("@placa", StringExtensions.NullString(txtPlaca.Text.Trim()));
+                        cmd.Parameters.AddWithValue("@notas", StringExtensions.NullString(txtNotas.Text.Trim()));
+
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+
+                        MessageBox.Show("Los datos del vehículo han sido modificados.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se ha presentado un error.\nDetalles:\n\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void EliminarVehiculo(string id)
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(ConexionBD.connstring))
+                {
+                    using (SQLiteCommand cmd = new SQLiteCommand("DELETE FROM vehiculo WHERE id=@id", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@id", id);
+
+                        conn.Open();
+                        cmd.ExecuteReader();
+
+                        MessageBox.Show("Los datos del vehículo han sido eliminados.");
+                        RestaurarCampos();
+                        CargarCedulas();
+                        //RestaurarCampos();
+                        //CargarVehiculosPorCedula(comboCedula.Text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se ha presentado un error.\nDetalles:\n\n" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void RestaurarCampos()
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is TextBox || c is RichTextBox)
+                {
+                    c.Text = "";
+                }
+            }
+            checkCamion.Checked = false;
+            btnEliminar.Visible = false;
+            btnModificar.Visible = false;
+            //comboVehiculos.SelectedIndex = -1;
+        }
+
+        private void comboVehiculos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if (comboVehiculos.SelectedIndex >= 0)
+            //{
+                CargarDatosVehiculo(idVehiculo[comboVehiculos.SelectedIndex].ToString());
+                //MessageBox.Show(idVehiculo[comboVehiculos.SelectedIndex].ToString());
+
+                btnEliminar.Visible = true;
+                btnModificar.Visible = true;
+            //}
+        }
+
+        private void comboCedula_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RestaurarCampos();
+            CargarVehiculosPorCedula(comboCedula.Text);
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            ModificarVehiculo(idVehiculo[comboVehiculos.SelectedIndex].ToString());
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            msg = new Msg();
+            msg.lblMsg.Text = "¿Está seguro de que desea eliminar el registro?";
+            DialogResult dlgres = msg.ShowDialog();
+            {
+                if (dlgres == DialogResult.Yes)
+                {
+                    EliminarVehiculo(idVehiculo[comboVehiculos.SelectedIndex].ToString());
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
