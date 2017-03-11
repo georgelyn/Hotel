7abff1e052907f64898f6bcbc90caf170658db01
georgelyn <30326226+georgelyn@users.noreply.github.com>
2017-03-11 13:23:27 -0400

    Base de datos: Hacer copia, restaurarla, restablecer la base de datos y crear una nueva.

---
+++ Hotel/CrearBaseDatos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel
+{
+    class CrearBaseDatos
+    {
+        public static string query = @"CREATE TABLE IF NOT EXISTS [Clientes] (
+                          [ID] INTEGER PRIMARY KEY AUTOINCREMENT,
+                        [Nombre] VARCHAR (300) NOT NULL,
+                        [Cedula] VARCHAR(100) NOT NULL UNIQUE,
+                        [Edad] VARCHAR(10),
+                        [Telefono] VARCHAR(50),
+                        [TelefonoExtra] VARCHAR(50),
+                        [ClienteDesde] DATE,
+                        [Notas] VARCHAR(500)
+                    );
+
+                        CREATE TABLE IF NOT EXISTS [Habitaciones] (
+                        [ID] INTEGER PRIMARY KEY AUTOINCREMENT,
+                        [NumeroHabitacion] INTEGER UNIQUE,
+                        [Estado] VARCHAR (50) NOT NULL
+                    );
+
+                        CREATE TABLE IF NOT EXISTS [Vehiculos] (
+                        [ID] INTEGER  PRIMARY KEY AUTOINCREMENT,
+                        [Cliente_Cedula] VARCHAR (100) REFERENCES Clientes (Cedula) ON DELETE CASCADE
+                                                                                    ON UPDATE CASCADE,
+                        [EsCamion] BOOLEAN,
+                        [Marca] VARCHAR (300),
+                        [Modelo] VARCHAR (200),
+                        [Placa] VARCHAR (100),
+                        [Notas] VARCHAR (300) 
+                    );
+
+                        CREATE TABLE IF NOT EXISTS [Reservaciones] (
+                        [ID] INTEGER PRIMARY KEY AUTOINCREMENT,
+                        [Cliente_Cedula] VARCHAR (100) REFERENCES Clientes(Cedula) ON DELETE CASCADE
+                                                                                   ON UPDATE CASCADE,
+                        [Vehiculo_ID] INTEGER REFERENCES Vehiculos(ID) ON DELETE SET NULL,
+                        [NumeroHabitacion] INTEGER  REFERENCES Habitaciones(NumeroHabitacion) UNIQUE,
+                        [FechaIngreso] DATE,
+                        [FechaSalida] DATE,
+                        [TipoHabitacion] VARCHAR(200),
+                        [CiudadOrigen] VARCHAR(100),
+                        [CiudadDestino] VARCHAR(100),
+                        [CostoTotal] DECIMAL,
+                        [Notas] VARCHAR(500)
+                    );
+
+                        CREATE TABLE IF NOT EXISTS [TipoHabitacion] (
+                        [ID] INTEGER PRIMARY KEY AUTOINCREMENT,
+                        [Tipo] VARCHAR (300),
+                        [Descripcion] VARCHAR (300),
+                        [Costo] DECIMAL,
+                        [Notas] VARCHAR (500),
+                        [Activa] BOOLEAN
+                    );
+
+                        CREATE TABLE IF NOT EXISTS Seguridad (
+                        [ID] INTEGER PRIMARY KEY AUTOINCREMENT,
+                        [User] VARCHAR,
+                        [Password] VARCHAR
+                    )";
+    }
+}
--- Hotel/SQLite.cs
+++ Hotel/SQLite.cs
@@ -18,7 +18,7 @@ namespace Hotel
             InitializeComponent();
         }
 
-        private void RealizarCopia()
+        private void HacerCopia()
         {
             string fechaActual = DateTime.Now.ToString("yyyy-MM-dd_hh-mm");
             string nombre = "HotelBD-" + fechaActual + ".db";
@@ -43,20 +43,6 @@ namespace Hotel
                     original.BackupDatabase(respaldo, "main", "main", -1, null, 0);
                 }
             }
-
-            //string fechaActual = DateTime.Now.ToString("yyyy-MM-dd_hh-mm");
-            //string nombre = "HotelBD-" + fechaActual + ".db";
-            //string ubicacion = Application.StartupPath + "\\Respaldos\\" + nombre;
-
-            //string query = @"Data Source=|DataDirectory|" + ubicacion + ";Version=3;New=True;Compress=True;Foreign Keys=ON";
-
-            //using (var original = new SQLiteConnection(ConexionBD.connstring))
-            //using (var respaldo = new SQLiteConnection(query))
-            //{
-            //    original.Open();
-            //    respaldo.Open();
-            //    original.BackupDatabase(respaldo, "main", "main", -1, null, 0);
-            //}
         }
 
         private void RestaurarCopia()
@@ -73,8 +59,9 @@ namespace Hotel
             if (dr == DialogResult.OK)
             {
                 string respaldo = openFileDialog1.FileName;
+                string nuevaConexion = "Data Source=" + respaldo + ";Version=3;New=True;Compress=True;Foreign Keys=ON";
 
-                if (ProbarConexion(respaldo))
+                if (ProbarConexion(nuevaConexion))
                 {
                     Msg msg = new Msg();
 
@@ -83,8 +70,10 @@ namespace Hotel
                     {
                         if (dlgres == DialogResult.Yes)
                         {
-                            ActualizarConexion(respaldo);
-
+                            if (ActualizarConexion(respaldo))
+                            {
+                                MessageBox.Show("La base de datos ha sido restaurada.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
                             //if (ProbarConexion())
                             //{
                             //    MessageBox.Show("Respaldo restaurado");
@@ -96,10 +85,60 @@ namespace Hotel
                         }
                     }
                 }
-                else
+                //else
+                //{
+                //    MessageBox.Show("El archivo de base de datos no es válido. Por favor, verifique.");
+                //}
+            }
+        }
+
+        private void CrearNuevaBaseDatos()
+        {
+            try
+            {
+                System.Data.SQLite.SQLiteConnection.CreateFile("HotelCountry.db"); // Crear el archivo. No realmente necesario, ya que SQLite lo crear automáticamente si no lo encuentra
+
+                using (System.Data.SQLite.SQLiteConnection conn = new System.Data.SQLite.SQLiteConnection(@"Data Source=|DataDirectory|\HotelCountry.db;Version=3;New=True;Compress=True;Foreign Keys=ON"))
                 {
-                    MessageBox.Show("El archivo de base de datos no es válido. Por favor, verifique.");
+                    using (System.Data.SQLite.SQLiteCommand cmd = new System.Data.SQLite.SQLiteCommand(conn))
+                    {
+                        conn.Open(); 
+
+                        cmd.CommandText = CrearBaseDatos.query;
+                        cmd.ExecuteNonQuery();
+
+                        cmd.CommandText = "INSERT INTO TipoHabitacion (Tipo, Costo, Activa) VALUES ('Matrimonial','15000', '1'), ('Doble', '16000', '1'), ('Triple', '18000', '1'), " +
+                            "('Cuádruple', '20000', '1'), ('Séxtuple', '25000', '1'), ('Mini-Suite', '25000', '1')";    
+                        cmd.ExecuteNonQuery();
+
+                        cmd.CommandText = "INSERT INTO Seguridad (User, Password) VALUES ('admin','ra0408'), ('usuario', '2506'), ('admin', 'emergencia')"; 
+                        cmd.ExecuteNonQuery();
+                    }
                 }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se ha presentado un problema.\n\nDetalles: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void RestablecerBase()
+        {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection("Data Source=|DataDirectory|\\Respaldos\\Hotel-10-03-2017-04-31.db"))
+                {
+                    using (SQLiteCommand cmd = new SQLiteCommand("DELETE FROM Clientes; DELETE FROM Reservaciones; DELETE FROM Vehiculos; UPDATE Habitaciones SET Estado='disponible'", conn))
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se ha presentado un problema.\n\nDetalles: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -112,102 +151,80 @@ namespace Hotel
                     using (SQLiteCommand cmd = new SQLiteCommand("SELECT Nombre FROM Clientes", conn))
                     {
                         conn.Open();
-                        return true;
+
+                        using (SQLiteDataReader dr = cmd.ExecuteReader())
+                        {
+                            if (dr.HasRows)
+                            {
+                                return true;
+                            }
+                        }
                     }
                         
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error", ex.Message);
+                if (ex.Message.Contains("not a database"))
+                {
+                    MessageBox.Show("El archivo de base de datos no es válido. Por favor, verifique.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Se ha presentado un problema.\n\nDetalles: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
             return false;
         }
 
-        //private bool ProbarConexion()
-        //{
-        //    try
-        //    {
-        //        using (SQLiteConnection conn = new SQLiteConnection(ConexionBD.connstring))
-        //        {
-        //            conn.Open();
-        //            return true;
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show("Error", ex.Message);
-        //    }
-        //    return false;
-        //}
-
-        private void ActualizarConexion(string ubicacion)
+        private bool ActualizarConexion(string ubicacion)
         {
-            string stringConnection = "Data Source=" + ubicacion + ";Version=3;New=True;Compress=True;Foreign Keys=ON";
+            try
+            {
+                string stringConnection = "Data Source=" + ubicacion + ";Version=3;New=True;Compress=True;Foreign Keys=ON";
 
-            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
-            config.ConnectionStrings.ConnectionStrings["connstring"].ConnectionString = stringConnection;
+                config.ConnectionStrings.ConnectionStrings["connstring"].ConnectionString = stringConnection;
 
-            config.Save(ConfigurationSaveMode.Modified);
+                config.Save(ConfigurationSaveMode.Modified);
 
-            ConfigurationManager.RefreshSection("connectionStrings");
+                ConfigurationManager.RefreshSection("connectionStrings");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("La base de datos no fue restaurada.\n\nDetalles: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            RealizarCopia();
-            //RealizarCopia();
+            HacerCopia();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
 
             RestaurarCopia();
-
-            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
-
-            //openFileDialog1.Filter = "Archivo de base da datos (*.db, *.sqlite, *.sqlite3) | *.db; *.sqlite; *.sqlite3";
-            //openFileDialog1.FilterIndex = 1;
-            //openFileDialog1.InitialDirectory = Application.StartupPath + "\\Respaldos";
-
-            //openFileDialog1.Multiselect = false;
-
-            //DialogResult dr = openFileDialog1.ShowDialog();
-            //if (dr == DialogResult.OK)
-            //{
-            //    string respaldo = openFileDialog1.FileName;
-
-            //    Msg msg = new Msg();
-
-            //    msg.lblMsg.Text = $"¿Está seguro de que desea restaurar la copia?\nNota: Los datos actuales se perderán.";
-            //    DialogResult dlgres = msg.ShowDialog();
-            //    {
-            //        if (dlgres == DialogResult.Yes)
-            //        {
-            //            ActualizarConexion(respaldo);
-
-            //            if (ProbarConexion())
-            //            {
-            //                MessageBox.Show("Respaldo restaurado");
-            //            }
-            //        }
-            //        else
-            //        {
-            //            return;
-            //        }
-            //    }
-            //}
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            MessageBox.Show(ConfigurationManager.ConnectionStrings["connstring"].ConnectionString);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             MessageBox.Show(ConfigurationManager.ConnectionStrings["connstring"].ConnectionString);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            RestablecerBase();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            CrearNuevaBaseDatos();
         }
     }
 }
