fc09f6b12f78f671be0bddf111ccd56605ff2e90
georgelyn <30326226+georgelyn@users.noreply.github.com>
2017-03-11 10:51:05 -0400

    Backup y Restauración

--- Hotel/SQLite.Designer.cs
+++ Hotel/SQLite.Designer.cs
@@ -31,11 +31,13 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(71, 68);
+            this.button1.Location = new System.Drawing.Point(231, 268);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -45,7 +47,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(71, 143);
+            this.button2.Location = new System.Drawing.Point(325, 268);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -55,19 +57,37 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(263, 165);
+            this.button3.Location = new System.Drawing.Point(175, 147);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Conexion";
+            this.button3.Text = "Probar";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(77, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Conexion";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(261, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 20);
+            this.textBox1.TabIndex = 4;
             // 
             // SQLite
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(424, 316);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -75,6 +95,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SQLite";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,5 +104,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Hotel/SQLite.cs
+++ Hotel/SQLite.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.SQLite;
+using System.IO;
 
 namespace Hotel
 {
@@ -17,36 +18,103 @@ namespace Hotel
             InitializeComponent();
         }
 
-        private void Backup()
+        private void RealizarCopia()
         {
             string fechaActual = DateTime.Now.ToString("yyyy-MM-dd_hh-mm");
             string nombre = "HotelBD-" + fechaActual + ".db";
-            string ubicacion = Application.StartupPath + "\\Respaldos\\" + nombre;
+            //string ubicacion = Application.StartupPath + "\\Respaldos\\" + nombre;
 
-            string query = @"Data Source=|DataDirectory|" + ubicacion + ";Version=3;New=True;Compress=True;Foreign Keys=ON";
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = Application.StartupPath + "\\Respaldos";
+            saveFileDialog1.Filter = "Archivo de base de datos|*.db";
+            saveFileDialog1.Title = "Guardar copia de seguridad";
+            saveFileDialog1.FileName = nombre;
+            DialogResult dr = saveFileDialog1.ShowDialog();
 
-            using (var original = new SQLiteConnection(ConexionBD.connstring))
-            using (var respaldo = new SQLiteConnection(query))
+            if (dr == DialogResult.OK)
             {
-                original.Open();
-                respaldo.Open();
-                original.BackupDatabase(respaldo, "main", "main", -1, null, 0);
+                string query = @"Data Source=|DataDirectory|" + saveFileDialog1.FileName + ";Version=3;New=True;Compress=True;Foreign Keys=ON";
+
+                using (var original = new SQLiteConnection(ConexionBD.connstring))
+                using (var respaldo = new SQLiteConnection(query))
+                {
+                    original.Open();
+                    respaldo.Open();
+                    original.BackupDatabase(respaldo, "main", "main", -1, null, 0);
+                }
+            }
+
+            //string fechaActual = DateTime.Now.ToString("yyyy-MM-dd_hh-mm");
+            //string nombre = "HotelBD-" + fechaActual + ".db";
+            //string ubicacion = Application.StartupPath + "\\Respaldos\\" + nombre;
+
+            //string query = @"Data Source=|DataDirectory|" + ubicacion + ";Version=3;New=True;Compress=True;Foreign Keys=ON";
+
+            //using (var original = new SQLiteConnection(ConexionBD.connstring))
+            //using (var respaldo = new SQLiteConnection(query))
+            //{
+            //    original.Open();
+            //    respaldo.Open();
+            //    original.BackupDatabase(respaldo, "main", "main", -1, null, 0);
+            //}
+        }
+
+        private void RestaurarCopia()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "Archivo de base da datos (*.db, *.sqlite, *.sqlite3) | *.db; *.sqlite; *.sqlite3";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.InitialDirectory = Application.StartupPath + "\\Respaldos";
+
+            openFileDialog1.Multiselect = false;
+
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string respaldo = openFileDialog1.FileName;
+
+                if (ProbarConexion(respaldo))
+                {
+                    Msg msg = new Msg();
+
+                    msg.lblMsg.Text = $"¿Está seguro de que desea restaurar la copia?\nNota: Los datos actuales se perderán.";
+                    DialogResult dlgres = msg.ShowDialog();
+                    {
+                        if (dlgres == DialogResult.Yes)
+                        {
+                            ActualizarConexion(respaldo);
+
+                            //if (ProbarConexion())
+                            //{
+                            //    MessageBox.Show("Respaldo restaurado");
+                            //}
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El archivo de base de datos no es válido. Por favor, verifique.");
+                }
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Backup();
-        }
-
-        private bool ProbarConexion()
+        private bool ProbarConexion(string connstring)
         {
             try
             {
-                using (SQLiteConnection conn = new SQLiteConnection(ConexionBD.connstring))
+                using (SQLiteConnection conn = new SQLiteConnection(connstring))
                 {
-                    conn.Open();
-                    return true;
+                    using (SQLiteCommand cmd = new SQLiteCommand("SELECT Nombre FROM Clientes", conn))
+                    {
+                        conn.Open();
+                        return true;
+                    }
+                        
                 }
             }
             catch (Exception ex)
@@ -56,10 +124,26 @@ namespace Hotel
             return false;
         }
 
+        //private bool ProbarConexion()
+        //{
+        //    try
+        //    {
+        //        using (SQLiteConnection conn = new SQLiteConnection(ConexionBD.connstring))
+        //        {
+        //            conn.Open();
+        //            return true;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("Error", ex.Message);
+        //    }
+        //    return false;
+        //}
+
         private void ActualizarConexion(string ubicacion)
         {
-            string nuevaUbicacion = ubicacion;
-            string stringConnection = "Data Source=|" + nuevaUbicacion + ";Version=3;New=True;Compress=True;Foreign Keys=ON";
+            string stringConnection = "Data Source=" + ubicacion + ";Version=3;New=True;Compress=True;Foreign Keys=ON";
 
             Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 
@@ -70,47 +154,60 @@ namespace Hotel
             ConfigurationManager.RefreshSection("connectionStrings");
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RealizarCopia();
+            //RealizarCopia();
+        }
+
         private void button2_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
-            openFileDialog1.Filter = "Archivo de base da datos (.db)|*.db|All Files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 1;
+            RestaurarCopia();
 
-            openFileDialog1.Multiselect = false;
+            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
-            DialogResult dr = openFileDialog1.ShowDialog();
-            if (dr == DialogResult.OK)
-            {
-                string respaldo = openFileDialog1.FileName;
+            //openFileDialog1.Filter = "Archivo de base da datos (*.db, *.sqlite, *.sqlite3) | *.db; *.sqlite; *.sqlite3";
+            //openFileDialog1.FilterIndex = 1;
+            //openFileDialog1.InitialDirectory = Application.StartupPath + "\\Respaldos";
 
-                Msg msg = new Msg();
+            //openFileDialog1.Multiselect = false;
 
-                msg.lblMsg.Text = $"¿Está seguro de que desea restaurar la copia?\nNota: Los datos actuales se perderán.";
-                DialogResult dlgres = msg.ShowDialog();
-                {
-                    if (dlgres == DialogResult.Yes)
-                    {
-                        ConexionBD.nuevaUbicacion = respaldo;
-                        ActualizarConexion(respaldo);
+            //DialogResult dr = openFileDialog1.ShowDialog();
+            //if (dr == DialogResult.OK)
+            //{
+            //    string respaldo = openFileDialog1.FileName;
 
-                        if (ProbarConexion())
-                        {
-                            MessageBox.Show("Respaldo restaurado");
-                        }
-                    }
-                    else
-                    {
-                        return;
-                    }
-                }
-            }
+            //    Msg msg = new Msg();
+
+            //    msg.lblMsg.Text = $"¿Está seguro de que desea restaurar la copia?\nNota: Los datos actuales se perderán.";
+            //    DialogResult dlgres = msg.ShowDialog();
+            //    {
+            //        if (dlgres == DialogResult.Yes)
+            //        {
+            //            ActualizarConexion(respaldo);
+
+            //            if (ProbarConexion())
+            //            {
+            //                MessageBox.Show("Respaldo restaurado");
+            //            }
+            //        }
+            //        else
+            //        {
+            //            return;
+            //        }
+            //    }
+            //}
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(ConexionBD.nuevaUbicacion);
-            MessageBox.Show(ConexionBD.connstring);
+            MessageBox.Show(ConfigurationManager.ConnectionStrings["connstring"].ConnectionString);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(ConfigurationManager.ConnectionStrings["connstring"].ConnectionString);
         }
     }
 }
